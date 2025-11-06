9135a24f4ab418820555d4fefdad4ce7abd8462d
cal <126645272+cal-1103@users.noreply.github.com>
2025-11-06 15:05:22 -0500

    another bugfix

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-const registerLink = document.getElementById('register-btn');
+document.addEventListener('DOMContentLoaded', () => {
+  const registerLink = document.getElementById('register-btn');
 
-registerLink.addEventListener('click', (event) => {
-  event.preventDefault(); // stops the link from refreshing the page
-  console.log("Register clicked!"); // test that click works
+  registerLink.addEventListener('click', (event) => {
+    event.preventDefault(); // stops the link from refreshing the page
+    console.log("Register clicked!"); // test
+  });
 });
