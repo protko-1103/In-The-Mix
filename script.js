18975b1cc135e0d773d5a9ea87b362c287c18b0c
cal <126645272+cal-1103@users.noreply.github.com>
2025-11-06 15:02:33 -0500

    Saved file

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ const registerLink = document.getElementById('registerLink');
 
 registerLink.addEventListener('click', (event) => {
   event.preventDefault(); // stops the link from refreshing the page
-  console.log("Register clicked!"); // test that click works
+  console.log("Register clicked!"); // test that click workss
 });
